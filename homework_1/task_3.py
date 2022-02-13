bfd43a806668676c9b5447d7ea6aadf23b7160f2
Valentina-Malchikova <98917435+Valentina-Malchikova@users.noreply.github.com>
2022-02-13 22:54:52 +0300

    Delete homework_1 directory

--- homework_1/task_3.py
+++
@@ -1,7 +0,0 @@
-# Узнайте у пользователя число n.
-# Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-number = int(input('Введите число: '))
-formula = (number + int(str(number) + str(number)) + int(str(number) + str(number) + str(number)))
-print(formula)
