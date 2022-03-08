2920f68683edaf8f2ca5f0c1842ce04aab1b28bc
Valentina_M <malyavo4ka6@gmail.com>
2022-03-08 19:20:39 +0300

    homework_6 added

---
+++ homework_6/task_3.py
@@ -0,0 +1,29 @@
+# Реализовать базовый класс Worker (работник).
+# определить атрибуты: name, surname, position (должность), income (доход);
+# последний атрибут должен быть защищённым и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus};
+# создать класс Position (должность) на базе класса Worker;
+# в классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+# и дохода с учётом премии (get_total_income);
+# проверить работу примера на реальных данных:
+# создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров.
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
+
+class Position(Worker):
+    def get_full_name(self):
+        return '{0} {1}'.format(self.name, self.surname)
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+employee = Position('Margaret', 'Tetcher', 'prime minister', 60000, 100000)
+print(employee.name)
+print(employee.surname)
+print(employee._income)
+print(employee.get_full_name())
+print(employee.get_total_income())
